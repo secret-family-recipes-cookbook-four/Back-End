2fce76df83978baad131fe35c41a02692d74cdf6
Matthew Kearney <matthewkearney@matthews-mbp.lan>
2020-05-27 16:12:04 -0500

    added test for login

--- users/auth_router.spec.js
+++ users/auth_router.spec.js
@@ -1,6 +1,8 @@
 const request = require('supertest');
 const server = require('../api/server.js');
 
+
+//register test
 describe('POST to register', () => {
 
     it('should return 201 on valid register', () => {
@@ -20,3 +22,25 @@ describe('POST to register', () => {
         });
     });
 })
+
+
+//login test
+describe('POST /login', () => {
+    it('should return 401 when missing username and password', () => {
+        return request(server)
+            .post('/api/auth/login')
+            .send({ username: '', password: '' })
+            .then(res => {
+                expect(res.status).toBe(401)
+            })
+    })
+    it("should return 500 to invalid password", () => {
+        return request(server)
+        .post("/api/auth/register")
+        .send({ username: 'username', password: 5 })
+        .then(res => {
+        expect(res.status).toBe(500);
+        });
+    });
+})
+ 
